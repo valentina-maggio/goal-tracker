d3b7520c613bda0e5a451236e259efcd1ae6f170
Valentina <maggio.vale@gmail.com>
2022-05-25 17:26:25 +0200

    Setting up routes
    - get, post, put, delete routes

--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/api/goals', (req, res) => {
-  res.status(200).json({ message: 'Get goals' });
-});
+app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
