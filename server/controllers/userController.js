c729159abb3de21f7588310f99cb0d9007c93dd8
Valentina <maggio.vale@gmail.com>
2022-05-29 18:01:38 +0200

    Add json token to generate encoded token for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,7 +35,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400);
@@ -55,6 +56,7 @@ const loginUser = asyncHandler(async(req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400);
@@ -66,6 +68,11 @@ const loginUser = asyncHandler(async(req, res) => {
 const getUser = asyncHandler(async (req, res) => {
   res.json({ message: 'User data display' })
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
 
 module.exports = {
   registerUser,
