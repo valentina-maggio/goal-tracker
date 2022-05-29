127268fa1bfd2cfaa6358ba03e2191971c0624dd
Valentina <maggio.vale@gmail.com>
2022-05-29 14:20:35 +0200

    Add express async handler
    - new dependency installed
    - to manage promises when requesting data from DB
    - no need to use try catch

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,27 +1,29 @@
+const asyncHandler = require('express-async-handler');
+
 // Get goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals' });
-};
+});
 
 // Set goals
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('Please add text field');
   }
 
   res.status(200).json({ message: 'Set goals' });
-};
+});
 
 // Update goals
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // Delete goals
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 
 module.exports = {
