285e72615e5d8e00aa8ba86344701659968bb008
Valentina <maggio.vale@gmail.com>
2022-05-29 13:45:41 +0200

    Add more controllers
    - for post, put, delete routes

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,7 +1,27 @@
+// Get goals
 const getGoals = (req, res) => {
   res.status(200).json({ message: 'Get goals' });
-}
+};
+
+// Set goals
+const setGoals = (req, res) => {
+  res.status(200).json({ message: 'Set goals' });
+};
+
+// Update goals
+const updateGoals = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// Delete goals
+const deleteGoals = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
 
 module.exports = {
   getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
 }
