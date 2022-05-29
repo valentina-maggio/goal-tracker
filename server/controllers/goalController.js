4a3823e718244eaa4c1f0b9e2e2f924a95fa0070
Valentina <maggio.vale@gmail.com>
2022-05-29 16:11:11 +0200

    Update controller method updateGoal to update existing goal

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -25,7 +25,18 @@ const setGoal = asyncHandler(async (req, res) => {
 
 // Update goals
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if(!goal) {
+    res.status(400)
+    throw new Error('Goal not found');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { 
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // Delete goals
