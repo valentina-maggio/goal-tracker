aec3591aaceeaa117f794c3d3ef360921b8bc120
Valentina <maggio.vale@gmail.com>
2022-05-29 18:26:09 +0200

    Authentication middleware
    - retrieve user after token verification
    - protect user routes if no recognsied token

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,9 +5,10 @@ const {
   loginUser, 
   getUser 
 } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/user', getUser);
+router.get('/user', protect, getUser);
 
 module.exports = router;
