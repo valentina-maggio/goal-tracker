7df106c268b8e54b6c4f562693e0d43340832851
Valentina <maggio.vale@gmail.com>
2022-05-29 21:14:18 +0200

    Add protection to goal route
    - update controller methods getGoals and setGoal to filter based on the user id

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -4,7 +4,7 @@ const Goal = require('../models/goalModel');
 
 // Get goals
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goals);
 });
@@ -17,7 +17,8 @@ const setGoal = asyncHandler(async (req, res) => {
   }
 
   const goal = await Goal.create({
-    text: req.body.text
+    text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
