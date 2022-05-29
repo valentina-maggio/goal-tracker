ab148a3a3cb46acfe31e971a19720edef0566d59
Valentina <maggio.vale@gmail.com>
2022-05-29 15:53:21 +0200

    Setting up models
    - goal model creation

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel');
+
 // Get goals
 const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get goals' });
