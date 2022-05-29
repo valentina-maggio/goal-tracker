557b10e683be5f2675549f7aa03680e4cbb24361
Valentina <maggio.vale@gmail.com>
2022-05-29 17:53:26 +0200

    Implement user login functionality
    - use bcrypt to compare passwords and retrieve user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,8 +45,22 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // Login user
 const loginUser = asyncHandler(async(req, res) => {
-  res.json({ message: 'Login User' })
-});
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials')
+    }
+  });
 
 // Get user data
 const getUser = asyncHandler(async (req, res) => {
