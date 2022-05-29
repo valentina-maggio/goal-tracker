bd47ea3c361e26fe45a6bafe0fcbcfcf93ca2b38
Valentina <maggio.vale@gmail.com>
2022-05-29 14:01:22 +0200

    Setting up error handler for requested data

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -5,6 +5,11 @@ const getGoals = (req, res) => {
 
 // Set goals
 const setGoal = (req, res) => {
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add text field');
+  }
+
   res.status(200).json({ message: 'Set goals' });
 };
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Middlewares to use the requested body data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
