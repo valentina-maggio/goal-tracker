23f841175a656309fcdc1b2af28efd1aaaf0ba68
Valentina <maggio.vale@gmail.com>
2022-05-31 11:53:26 +0200

    Adding semicolons

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -4,10 +4,10 @@ const API_URL = '/api/users/';
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
 
   if(response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem('user', JSON.stringify(response.data));
   }
 
   return response.data;
