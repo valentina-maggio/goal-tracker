77762e9b38ece20cd15ff7b86b8da8dea0b39d8d
Valentina <maggio.vale@gmail.com>
2022-06-01 19:08:44 +0200

    Create goal async thunk and extra reducer

--- client/src/components/GoalForm.jsx
+++ client/src/components/GoalForm.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createGoal } from '../features/goals/goalSlice';
 
 function GoalForm() {
   const [text, setText] = useState('');
 
+  const dispatch = useDispatch();
 
   const onSubmit = e => {
     e.preventDefault()
+
+    dispatch(createGoal({text}));
+    setText('');
   }
 
   return (
