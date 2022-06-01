42f484c2e77259d5fb24a665161a38601f8630e3
Valentina <maggio.vale@gmail.com>
2022-06-01 18:47:17 +0200

    Setting up goal form to create new goals

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import GoalForm from '../components/GoalForm';
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -14,9 +15,14 @@ function Dashboard() {
   }, [user, navigate])
 
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
   )
 }
 
