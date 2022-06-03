5ef6638fe8961917e98a7a87a060a64166053854
Valentina <maggio.vale@gmail.com>
2022-06-03 12:06:54 +0200

    Display goals
    - GoalItem component to display goal with date
    - section in Dashboard page to display the goals

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import GoalForm from '../components/GoalForm';
+import GoalItem from '../components/GoalItem';
 import Spinner from '../components/Spinner';
 import { getGoals, reset } from '../features/goals/goalSlice';
 
@@ -10,7 +11,7 @@ function Dashboard() {
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
-  const { goal, isLoading, isError, message } = useSelector((state) => state.goals);
+  const { goals, isLoading, isError, message } = useSelector((state) => state.goals);
 
   useEffect(() => {
     if(isError) {
@@ -40,6 +41,17 @@ function Dashboard() {
       </section>
 
       <GoalForm />
+
+      <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal}/>
+            ))}
+          </div>
+        ) : (<h3>You have not set any goals</h3>)}
+
+      </section>  
     </>
   )
 }
